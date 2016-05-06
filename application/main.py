1ff8fc68b5d7475bb32a21748bde6be3732632c7
Robert MacGregor <ragoradev@gmail.com>
2016-05-06 04:55:53 -0400

    Adjust main.py to use an interval timer

--- application/main.py
+++ application/main.py
@@ -1,16 +1,16 @@
-""" 
+"""
     PyIRCBot is a bot written in Python that is designed to be simple to use.
-    
+
     All of the behavior is controlled by a single configuration file whose syntax
     is very user friendly and is commented to desiginate the exact functionality of
     each configuration value.
-    
+
     The technical design of the software is addon-oriented, therefore it is quite
     simple to add and remove functionality to the system as deemed necessary.
-    
+
     This software is licensed under the MIT license, refer to LICENSE.txt for
     more information.
-    
+
     Copyright (c) 2014 Robert MacGregor
 """
 
@@ -30,12 +30,12 @@ if (home_exists is False):
     os.system('mkdir %s' % home_path)
 
 # Setup the scheduler
-scheduler = BackgroundScheduler()    
+scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job('interval', seconds=2)
 def keepalive():
     client.send('PING :DRAGON\r\n')
-    
+scheduler.add_job(keepalive, "interval", seconds=2)
+
 # Setup the IRC client
 client = irc.Connection(home_path, scheduler)
 scheduler.start()
@@ -43,4 +43,3 @@ scheduler.start()
 # Begin the main loop
 while (True):
     client.receive()
-        
